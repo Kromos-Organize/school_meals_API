12f8a7ddcae2bef4580e1a5d968496163fc97459
Krom_oS1 <Roma@bezkassira.by>
2023-02-11 14:02:22 +0300

    update eco

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -7,9 +7,9 @@ module.exports = {
     },
     watch: true,
     log_date_format: 'DD-MM-YY HH:MM Z',
-    error_file: 'school_meals_dev/school_meals_API/logs',
-    log_file: 'school_meals_dev/school_meals_API/logs',
-    out_file: 'school_meals_dev/school_meals_API/logs',
+    error_file: '/home/kromos/school_meals_dev/school_meals_API/logs',
+    log_file: '/home/kromos/school_meals_dev/school_meals_API/logs',
+    out_file: '/home/kromos/school_meals_dev/school_meals_API/logs',
   }],
 
   deploy : {
@@ -18,8 +18,8 @@ module.exports = {
       user : 'kromos',
       host : '65.21.245.221',
       ref  : 'origin/main',
-      repo : 'https://github.com/Kromos-Organize/school_meals_API',
-      path : 'school_meals_dev/school_meals_API',
+      repo : 'git@github.com:Kromos-Organize/school_meals_API.git',
+      path : '/home/kromos/school_meals_dev/school_meals_API',
       'pre-deploy-local': '',
       'post-deploy' : 'git checkout . && git checkout main && git pull && npm install && npm run build && pm2 reload ecosystem.config.js --env production',
       'pre-setup': ''
