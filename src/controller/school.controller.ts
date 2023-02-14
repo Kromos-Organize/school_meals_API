b10ff0047550541c15a7c02abdd615d5cb5464ef
Krom_oS1 <Roma@bezkassira.by>
2023-02-14 13:42:24 +0300

    update controller school

--- src/controller/school.controller.ts
+++ src/controller/school.controller.ts
@@ -1,10 +1,10 @@
-import {Body, Controller, Get, Param, Post, UsePipes} from '@nestjs/common';
+import {Body, Controller, Delete, Get, Param, Post, Put, UsePipes} from '@nestjs/common';
 import {ApiOperation, ApiResponse, ApiTags} from "@nestjs/swagger";
 import {SchoolService} from "../service/school.service";
 import {CreateSchoolDto} from "../dto/create-school.dto";
 import {School} from "../model/school.model";
 import {ValidationBody} from "../pipes/valdationBody.pipe";
-import {ValidateParams} from "../pipes/validateParams.pipe";
+import {MessageDto} from "../dto/message.dto";
 
 @ApiTags('Школа')
 @Controller('school')
@@ -22,11 +22,10 @@ export class SchoolController {
 
     @ApiOperation({summary: 'Получить данные школы'})
     @ApiResponse({status: 200, type: School})
-    @UsePipes(ValidateParams)
-    @Get(':id')
-    get(@Param('id') id: number,) {
+    @Get(':school_id')
+    get(@Param('school_id') school_id: number) {
 
-        return this.schoolService.get(id);
+        return this.schoolService.get(school_id);
     }
 
     @ApiOperation({summary: 'Добавить школу'})
@@ -36,5 +35,22 @@ export class SchoolController {
     create(@Body() schoolDto: CreateSchoolDto) {
 
         return this.schoolService.createSchool(schoolDto)
+    }
+
+    @ApiOperation({summary: 'Изменить данные школы'})
+    @ApiResponse({status: 200, type: School})
+    @UsePipes(ValidationBody)
+    @Put(':school_id')
+    update(@Param('school_id') school_id: number, @Body() schoolDto: CreateSchoolDto) {
+
+        return this.schoolService.updateSchool(school_id, schoolDto);
+    }
+
+    @ApiOperation({summary: 'Удалить школу'})
+    @ApiResponse({status: 200, type: MessageDto})
+    @Delete(':school_id')
+    remove(@Param('school_id') school_id: string) {
+
+        return this.schoolService.removeSchool(school_id)
     }
 }
