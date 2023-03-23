879ad46d915128b4f9f032eecf6b3dfdaf5dd403
Sergey Bataykin <bataykin@gmail.com>
2023-03-23 16:17:15 +0200

    fix: revert cookie options back

--- src/helpers/cookie.config.ts
+++ src/helpers/cookie.config.ts
@@ -3,6 +3,6 @@ import {CookieOptions} from "express";
 export const cookieConfigToken: CookieOptions = {
     maxAge: 24*60*60*1000,
     httpOnly: true,
-    secure: false,
+    secure: true,
     sameSite: "none"
 }
